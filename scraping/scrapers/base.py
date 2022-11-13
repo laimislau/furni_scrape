6afaec2267622af25efbcd6612f30a3fc7c1c628
laimislau <laucius.laimis@yahoo.com>
2022-11-13 16:56:33 +0200

    updated base class to fit new methods

--- scraping/scrapers/base.py
+++ scraping/scrapers/base.py
@@ -11,29 +11,29 @@ class BaseScraper(ABC):
     __domain__: str = ""
       
     @abstractmethod
-    def _retrieve_items_links(self, results_count: int, keyword: str) -> List[FurnitureLink]:
+    def _retrieve_item_links(self, results_count: int, keyword: str) -> List[FurnitureLink]:
         """Method to get items links searching by keyword and save specifed number of results."""
         pass
 
     def _get_page_content(self, query: str) -> Optional[BeautifulSoup]:
         """Method to get needed content from search result page or whatever related page."""
-        resp = requests.get(f"{self.__domain__}/{query}")
+        resp = requests.get(query)
         if resp.status_code == 200:
             return BeautifulSoup(resp.content)
         raise Exception("Cannot get content. Site is unreachable.")
 
-    def _retrieve_recipe_info(self, link: FurnitureLink) -> Optional[Furniture]:
+    def _retrieve_furniture_info(self, link: FurnitureLink) -> Optional[Furniture]:
         pass
 
-    def scrape(self, recipes_count: int, keyword: str) -> List[Furniture]:
+    def scrape(self, results_count: int, keyword: str) -> List[Furniture]:
         try:
-            pages_count = math.ceil(recipes_count / self.__items_per_page__)
+            pages_count = math.ceil(results_count / self.__items_per_page__)
         except ZeroDivisionError:
-            raise Exception("Recipes per page is 0!")
-        recipe_links = self._retrieve_items_list(pages_count, keyword)
+            raise Exception("Forgot to set how many items is displayed per page.")
+        furniture_links = self._retrieve_item_links(results_count, keyword)
         scraped_recipes: List[Furniture] = []
-        for recipe_link in recipe_links:
-            scraped_recipe = self._retrieve_recipe_info(recipe_link)
+        for recipe_link in furniture_links:
+            scraped_recipe = self._retrieve_furniture_info(recipe_link)
             if scraped_recipe is not None:
                 scraped_recipes.append(scraped_recipe)
         return scraped_recipes
